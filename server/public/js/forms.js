a09d9c1ac4db7a0371502c5e546f866f10c71dfe
Qing Miao <cdutwhu@outlook.com>
2021-06-29 09:40:49 +1000

    * v-if controls each form. unfinished

--- server/public/js/forms.js
+++ server/public/js/forms.js
@@ -6,29 +6,45 @@ const emitter = getEmitter();
 
 function viform(proj, vi) {
 
-    vi.name = false;
-    vi.path = false;
-    vi.svrname = false;
-    vi.svrid = false;
-    vi.provider = false;
-    vi.inputfmt = false;
-    vi.alignmethod = false;
-    vi.levelmethod = false;
+    vi.natsstreaming = false;
+    vi.nias3 = false;
+    vi.benthos = false;
+    vi.reader = false;
+    vi.align = false;
+    vi.txtclassifier = false;
+    vi.level = false;
+    vi.weight = false;
+    vi.hub = false;
 
     switch (proj) {
         case "NatsStreaming":
-            vi.name = true;
-            vi.path = true;
+            vi.natsstreaming = true;
+            // console.log("here");
             break;
-        default:
-            vi.name = true;
-            vi.path = true;
-            vi.svrname = true;
-            vi.svrid = true;
-            vi.provider = true;
-            vi.inputfmt = true;
-            vi.alignmethod = true;
-            vi.levelmethod = true;
+        case "Nias3":
+            vi.nias3 = true;
+            break;
+        case "Benthos":
+            vi.benthos = true;
+            break;
+        case "Reader":
+            vi.reader = true;
+            break;
+        case "Align":
+            vi.align = true;
+            break;
+        case "TxtClassifier":
+            vi.txtclassifier = true;
+            break;
+        case "Level":
+            vi.level = true;
+            break;
+        case "Weight":
+            vi.weight = true;
+            break;
+        case "Hub":
+            vi.hub = true;
+            break;
     }
 }
 
@@ -51,14 +67,15 @@ export default {
         });
 
         let vi = Vue.reactive({
-            name: true,
-            path: true,
-            svrname: true,
-            svrid: true,
-            provider: true,
-            inputfmt: true,
-            alignmethod: true,
-            levelmethod: true,
+            natsstreaming: false,
+            nias3: false,
+            benthos: false,
+            reader: false,
+            align: false,
+            txtclassifier: false,
+            level: false,
+            weight: false,
+            hub: false,
         })
 
         let names_in = Vue.ref([]);
